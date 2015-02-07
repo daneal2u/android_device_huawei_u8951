6c777b48c0e8296d2a31d110ffa54e7df3ec4426
Kyle Harrison <khwebmail@gmail.com>
2015-02-07 14:56:29 +0000

    Update properties for L

--- u8951.mk
+++ u8951.mk
@@ -46,8 +46,8 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.confg.hw_appfsversion=U8951V4_4_SYSIMG \
-    ro.confg.hw_appsbootversion=U8951V4_4_APPSBOOT \
-    ro.confg.hw_appversion=U8951V4_4_KERNEL
+    ro.confg.hw_appfsversion=U8951V5_0_SYSIMG \
+    ro.confg.hw_appsbootversion=U8951V5_0_APPSBOOT \
+    ro.confg.hw_appversion=U8951V5_0_KERNEL
 
 $(call inherit-product-if-exists, vendor/huawei/u8951/u8951-vendor.mk)
